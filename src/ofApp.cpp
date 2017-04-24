d0941598640fcbccaa6ec311d4815bb572660256
狭間祐至 <hazamayuji@mba.local>
2017-04-24 20:29:08 +0900

    “a”を押した時にアニメーションが表現されるようになりました！

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,15 +1,13 @@
 #include "ofApp.h"
-int x = 30;
-int y = 10;
-
-
+float radius =30;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofBackground(0, 0, 0);
-    verdana.load("verdana.ttf",30);
+    //verdana.load("verdana.ttf",30);
+    font.load("arial.ttf", 72);
     sentences.push_back("");
-
+    ofSetCircleResolution(60);
 }
 
 //--------------------------------------------------------------
@@ -18,31 +16,55 @@ void ofApp::update(){
 }
 
 //--------------------------------------------------------------
+
+bool ofApp::isCountOver() {
+    return sentences.at(currentPos).length() > 115;
+}
+
+//--------------------------------------------------------------
+
 void ofApp::draw(){
      ofSetColor(255, 255, 255);
     int height = y;
-     if(count < 120){
-         for (string sentence : sentences) {
-             ofDrawBitmapString(sentence, x, height);
-             height += y;
-         }
+    
+    for (string sentence : sentences) {
+        ofDrawBitmapString(sentence, x, start_y + height);
+        height += y;
     }
+    
+    if(count == 1){
+        for(int i = 0; i < 5; i++){
+            ofSetColor(255,255,255);
+            font.drawString("a", ofGetWidth()/2 - 22, ofGetHeight()/2 + 20);
+            ofNoFill();
+            ofSetColor(ofRandom(100, 255), ofRandom(100, 255), ofRandom(100, 255));
+            ofDrawCircle(ofGetWidth()/2, ofGetHeight()/2, i * 10 * ofRandom(8,15) );
+        }
+    
+    }
+    
 }
-
-
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-        sentences.at(currentPos) += key;
-    if (key == 13) {
+    sentences.at(currentPos) += key;
+    
+    if (key == 13 || isCountOver()) {
         sentences.push_back("");
         currentPos++;
     }
+    
+    if(key == 'a'){
         count ++;
+    }
+    
 }
+
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-    
+    if(key == 'a'){
+        count --;
+    }
 
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,6 +9,8 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+    
+        bool isCountOver();
 
 		void keyPressed(int key);
 		void keyReleased(int key);
@@ -24,13 +26,13 @@ class ofApp : public ofBaseApp{
     
     std::vector<string> sentences;//動的配列
     
-    int currentPos = 0;//配列の最初は0
-    
-    
+    int currentPos = 0;//配列の最初は0 currentPos = 現在の位置
+    int x = 30;
+    int y = 10;
+
+    int start_y = 30;
     int count = 0;
     
-    
-    ofTrueTypeFont verdana;
-    ofTrueTypeFont architxt;
+    ofTrueTypeFont font;
 		
 };
