ec52d20f724b3b112a992529234966120c269975
狭間祐至 <hazamayuji@mba.local>
2017-04-27 17:59:49 +0900

    4/27 18:00 進捗です

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,60 +1,169 @@
 #include "ofApp.h"
-float radius =30;
+#include <windows.h>
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    ofBackground(0, 0, 0);
-    //verdana.load("verdana.ttf",30);
-    font.load("arial.ttf", 72);
-    sentences.push_back("");
-    ofSetCircleResolution(60);
+    ofBackground(0, 0, 0);//背景は黒
+    ofSetCircleResolution(60);//円の周りをきれいに
+    ofEnableAlphaBlending();//透明度を使用可能
+    ofSetFrameRate(60);//１秒に60枚画面を切り替え
+    
+    font.load("arial.ttf", 72);//フォントを使用(フォント,サイズ)
+    font_2.load("Anders.ttf", 72);
+    font_small.load("Anders.ttf", 50);
+    
+    sentences.push_back(u8"はざま");
+    
+    
+    /*-----------------------いっぱいの円-----------------------*/
+    for (int i=0; i<NUM; i++) {
+        loc_x[i] = ofRandom(0,ofGetWidth());
+        loc_y[i] = ofRandom(0,ofGetHeight());
+        speed_x[i] = ofRandom(-10,10);
+        speed_y[i] = ofRandom(-10,10);
+        radius_key[i] =ofRandom(4,40);
+        red[i] = ofRandom(0,255);
+        blue[i] = ofRandom(0,255);
+        green[i] = ofRandom(0,255);
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
-}
-
-//--------------------------------------------------------------
-
-bool ofApp::isCountOver() {
-    return sentences.at(currentPos).length() > 115;
-}
-
-//--------------------------------------------------------------
-
-void ofApp::draw(){
-     ofSetColor(255, 255, 255);
-    int height = y;
     
+    /*-----------------------いっぱいの円-----------------------*/
+    for (int i=0; i<NUM; i++) {
+        if (loc_x[i] < 0) {
+            loc_x[i] = 0;
+            speed_x[i] = speed_x[i]*-1;
+        }
+        if (loc_x[i] > ofGetWidth()) {
+            loc_x[i] = ofGetWidth();
+            speed_x[i] = speed_x[i]*-1;
+        }
+        if (loc_y[i] < 0) {
+            loc_y[i] = 0;
+            speed_y[i] = speed_y[i]*-1;
+        }
+        if (loc_y[i] > ofGetHeight()) {
+            loc_y[i] = ofGetHeight();
+            speed_y[i] = speed_y[i]*-1;
+        }
+        loc_x[i] = loc_x[i] + speed_x[i];
+        loc_y[i] = loc_y[i] + speed_y[i];
+    }
+
+}
+
+//--------------------------------------------------------------
+
+/*-----------------------115文字以上で改行のため-----------------------*/
+bool ofApp::isCountOver() {
+    return sentences.at(currentPos).length() > 110;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    ofSetColor(255, 255, 255);//白色
+    
+    /*-----------------------文字を書くためのBox-----------------------*/
+    ofNoFill();
+    ofDrawRectangle(87, 90, 850, 300);
+    
+    /*-----------------------Sendボタン-----------------------*/
+    ofNoFill();
+    ofDrawRectangle(341, 440, 341, 100);
+    font_small.drawString("Send", ofGetWidth()/2-90 , ofGetHeight()/2 + 135);
+    
+    /*-----------------------文字が描画される-----------------------*/
+    int height = y;
     for (string sentence : sentences) {
-        ofDrawBitmapString(sentence, x, start_y + height);
+        ofDrawBitmapString(sentence, x, start_y + height);//文字を描画
         height += y;
     }
     
-    if(count == 1){
+    
+    /*-----------------------"a"が押された時-----------------------*/
+    if(count_a == 1){
         for(int i = 0; i < 5; i++){
             ofSetColor(255,255,255);
-            font.drawString("a", ofGetWidth()/2 - 22, ofGetHeight()/2 + 20);
+            font_2.drawString("a", ofGetWidth()/2 - 22, ofGetHeight()/2 + 20);
+            
             ofNoFill();
             ofSetColor(ofRandom(100, 255), ofRandom(100, 255), ofRandom(100, 255));
             ofDrawCircle(ofGetWidth()/2, ofGetHeight()/2, i * 10 * ofRandom(8,15) );
         }
-    
+    }
+   
+    /*-----------------------"b"が押された時-----------------------*/
+    if(count_b == 1){
+        for(int i = 0; i < 5; i++){
+            ofSetColor(255,255,255);
+            font_2.drawString("b", ofGetWidth()/2 - 22, ofGetHeight()/2 + 20);
+            
+            ofNoFill();
+            ofSetColor(ofRandom(100, 255), ofRandom(100, 255), ofRandom(100, 255));
+            ofDrawTriangle(ofRandom(0,1024), ofRandom(0,768),ofRandom(0,1024), ofRandom(0,768), ofRandom(0,1024), ofRandom(0,768));
+           
+        }
     }
     
+    /*-----------------------"c"が押された時-----------------------*/
+    if(count_c == 1){
+        for(int i = 0; i < 5; i++){
+            ofSetColor(255,255,255);
+            font_2.drawString("c", ofGetWidth()/2 - 22, ofGetHeight()/2 + 20);
+            
+            ofNoFill();
+            ofSetColor(ofRandom(100, 255), ofRandom(100, 255), ofRandom(100, 255));
+            ofDrawRectangle(ofRandom(0,1024), ofRandom(0,768),ofRandom(0,1024), ofRandom(0,768));
+        }
+    }
+    
+    /*-----------------------"d"が押された時-----------------------*/
+    if(count_d == 1){
+        ofSetColor(255,255,255);
+        font_2.drawString("d", ofGetWidth()/2 - 22, ofGetHeight()/2 + 20);
+    
+        for (int i=0; i<NUM; i++) {
+            ofNoFill();
+            ofSetColor(red[i], green[i], blue[i],127);
+            ofDrawCircle(loc_x[i], loc_y[i], radius_key[i]);
+        }
+        
+    }
 }
+
+
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     sentences.at(currentPos) += key;
     
-    if (key == 13 || isCountOver()) {
+    /*--------------------"enter"が押された時改行--------------------*/
+    if (key == 13 || isCountOver()) {   //enterは13(?)
         sentences.push_back("");
         currentPos++;
     }
     
+    /*--------------------"a"が押された時--------------------*/
     if(key == 'a'){
-        count ++;
+        count_a ++;
+    }
+    
+    /*--------------------"b"が押された時--------------------*/
+    if(key == 'b'){
+        count_b ++;
+    }
+    
+    /*--------------------"c"が押された時--------------------*/
+    if(key == 'c'){
+        count_c ++;
+    }
+    
+    /*--------------------"d"が押された時--------------------*/
+    if(key == 'd'){
+        count_d ++;
     }
     
 }
@@ -62,8 +171,25 @@ void ofApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
+    
+    /*--------------------"a"がはなされた時--------------------*/
     if(key == 'a'){
-        count --;
+        count_a --;
+    }
+
+    /*--------------------"b"がはなされた時--------------------*/
+    if(key == 'b'){
+        count_b --;
+    }
+    
+    /*--------------------"c"がはなされた時--------------------*/
+    if(key == 'c'){
+        count_c --;
+    }
+    
+    /*--------------------"d"がはなされた時--------------------*/
+    if(key == 'd'){
+        count_d --;
     }
 
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,7 +1,11 @@
 #pragma once
-
 #include "ofMain.h"
+
+//editorの部分
 #include <vector>
+
+//いっぱいの円に必要
+#define NUM 300
 
 class ofApp : public ofBaseApp{
 
@@ -10,8 +14,7 @@ class ofApp : public ofBaseApp{
 		void update();
 		void draw();
     
-        bool isCountOver();
-
+        bool isCountOver();//改行のため
 		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
@@ -24,15 +27,38 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
     
+    /*-----------------------editorの部分-----------------------*/
     std::vector<string> sentences;//動的配列
-    
     int currentPos = 0;//配列の最初は0 currentPos = 現在の位置
-    int x = 30;
+    
+    int x = 100;
     int y = 10;
 
-    int start_y = 30;
-    int count = 0;
+    int start_y = 100;
     
+    
+    /*---------------------key_pressedで使用---------------------*/
+    int count_a = 0;
+    int count_b = 0;
+    int count_c = 0;
+    int count_d = 0;
+    
+    /*-----------------------いっぱいの円-----------------------*/
+    float loc_x[NUM];
+    float loc_y[NUM];
+    float speed_x[NUM];
+    float speed_y[NUM];
+    float radius_key[NUM];
+    float red[NUM];
+    float green[NUM];
+    float blue[NUM];
+    
+    /*-----------------------"a"の時必要-----------------------*/
+    float radius =30;
+    
+    
+    /*---------------------フォントについて---------------------*/
     ofTrueTypeFont font;
-		
+    ofTrueTypeFont font_2;//フォントを使用
+    ofTrueTypeFont font_small;
 };
